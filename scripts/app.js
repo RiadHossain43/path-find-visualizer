e473df743ab7f2163138da2ca7c339a5531c5a46
RiadHossain <riadhossain7464@gmail.com>
2020-06-19 16:06:59 +0600

    New commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 import * as Util from './util.js'
 import { generateGrid,handleDraw } from './grid.js'
 
+// initializing....
 const container = Util.eleQRY('.container')
 const NODE_SIZE = 20
 {
@@ -14,5 +15,29 @@ Util.set_style(container, {
 })
 
 
-let NODES = generateGrid(container, NODE_SIZE)
-handleDraw(NODES)
+
+const NODES = generateGrid(container, NODE_SIZE)
+
+function selectStartToEnd(){
+    let points = {
+        startSelected:false,
+        endSelected:false,
+        walldrawable:false
+    }
+    window.addEventListener('mousedown',e=>{
+        console.log(points.startSelected,points.endSelected)
+        if(!points.startSelected){
+            Util.set_style(e.target,{backgroundColor:'violet'})
+            points.startSelected = true
+            return
+        }
+        if(points.startSelected && !points.endSelected){
+            Util.set_style(e.target,{backgroundColor:'red'})
+            points.endSelected = true
+            points.walldrawable = true
+            return
+        }
+        if(points.walldrawable) handleDraw(NODES)
+    })
+}
+selectStartToEnd()
