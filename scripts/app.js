e812f70b42239902bb23c4dc9fab14855a53a0b3
RiadHossain <riadhossain7464@gmail.com>
2020-06-20 16:08:44 +0600

    NEW Update

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,14 @@
 import * as Util from './util.js'
-import { generateGrid,handleDraw } from './grid.js'
+import { generateGrid, handleDraw } from './grid.js'
 
+let START = 0
+let DESTINATION = 0
+let found = null
 // initializing....
 const container = Util.eleQRY('.container')
+const algo_btn = Util.eleQRY('.algo')
+const output = Util.eleQRY('.output')
+
 const NODE_SIZE = 20
 {
     container.height = Math.floor((window.innerHeight - 50) / NODE_SIZE) * NODE_SIZE
@@ -14,30 +20,117 @@ Util.set_style(container, {
     gridTemplateRows: `repeat(${Math.floor(container.height / NODE_SIZE)},${NODE_SIZE}px)`,
 })
 
-
-
 const NODES = generateGrid(container, NODE_SIZE)
 
-function selectStartToEnd(){
+function selectStartToEnd() {
     let points = {
-        startSelected:false,
-        endSelected:false,
-        walldrawable:false
+        startSelected: false,
+        endSelected: false,
+        walldrawable: false
     }
-    window.addEventListener('mousedown',e=>{
-        console.log(points.startSelected,points.endSelected)
-        if(!points.startSelected){
-            Util.set_style(e.target,{backgroundColor:'violet'})
+    container.addEventListener('mousedown', e => {
+        // console.log(points.startSelected,points.endSelected)
+        if (!points.startSelected) {
+            Util.set_style(e.target, { backgroundColor: 'violet' })
             points.startSelected = true
+            START = e.target.id
             return
         }
-        if(points.startSelected && !points.endSelected){
-            Util.set_style(e.target,{backgroundColor:'red'})
+        if (points.startSelected && !points.endSelected) {
+            Util.set_style(e.target, { backgroundColor: 'red' })
             points.endSelected = true
             points.walldrawable = true
+            DESTINATION = e.target.id
+            console.log(NODES[START].id, NODES[DESTINATION].id)
             return
         }
-        if(points.walldrawable) handleDraw(NODES)
+        if (points.walldrawable) handleDraw(NODES)
     })
 }
 selectStartToEnd()
+
+const RELAXED = []
+
+function sort_by_dist(neighbours){
+    for(let i =0 ;i<neighbours.length;i++){
+        for(let j=i+1;j<neighbours.length;j++){
+            if(neighbours[i].dist>neighbours[j].dist){
+                let temp = neighbours[i]
+                neighbours[i]=neighbours[j]
+                neighbours[j]=temp
+            }
+        }
+    }
+    return neighbours
+}
+
+function printNeighbours(nei){
+    for(let i =0 ;i<nei.length;i++)
+        console.log('loop',nei[i].dist)
+}
+
+function apply(inp) {
+    let neighbours = getNeighbour(inp)
+    neighbours = relaxNode(inp, neighbours)
+    let neighbours_dist = sort_by_dist(neighbours)
+    // console.log(neighbours_dist)
+    let nextNode
+    while(neighbours_dist.length && !found){
+        nextNode = neighbours_dist.shift()
+        if (nextNode.id == DESTINATION) {
+            found = nextNode
+            console.log(found.dist)
+            output.innerHTML = `${found.dist}`
+            found.innerHTML = `${found.dist}`
+            return found
+        }
+        if (RELAXED.length >= NODES.length) {
+            // console.log('fault')
+            return
+        }
+        apply(nextNode.id)
+    }
+    return
+    // if (neighbours_dist.length) {   
+    // }
+}
+
+
+function getNeighbour(i) {
+    let neighbours = []
+    for (let j = 0; j < NODES.length; j++) {
+        if ((NODES[j].row == NODES[i].row && NODES[j].col == NODES[i].col) || NODES[j].relaxed) continue
+        if (NODES[j].row >= NODES[i].row - 1 &&
+            NODES[j].row <= NODES[i].row + 1 &&
+            NODES[j].col >= NODES[i].col - 1 &&
+            NODES[j].col <= NODES[i].col + 1
+        ) {
+            neighbours.push(NODES[j])
+        }
+    }
+    return neighbours
+}
+
+
+function relaxNode(i, neighbours) {
+
+    for (let j = 0; j < neighbours.length; j++) {
+        if (NODES[i].dist + 1 < neighbours[j].dist) {
+            neighbours[j].dist = NODES[i].dist + 1
+            // neighbours[j].innerHTML =`${neighbours[j].dist}`
+            if (j == neighbours.length - 1) continue
+
+        }
+    }
+    NODES[i].relaxed = true
+    if(NODES[i]!==NODES[START])Util.set_style(NODES[i],{backgroundColor:'blue'})
+    RELAXED.push(NODES[i])
+    return neighbours
+}
+algo_btn.addEventListener('click', () => {
+    console.log(START, DESTINATION)
+    NODES[START].dist = 0
+    apply(START)
+})
+export { START, DESTINATION }
+
--- scripts/grid.js
+++ scripts/grid.js
@@ -1,11 +1,12 @@
 import * as Util from './util.js'
+import {START , DESTINATION} from './app.js'
 function generateGrid(container,NODE_SIZE){
     // let c = 0
     let NODE = []
     for(var row = 0;row<container.height/NODE_SIZE;row++){
         for(var col = 0;col<container.width/NODE_SIZE;col++ ){
             let node = Util.crtEle('div')
-            {node.row = row ,node.col = col}
+            {node.row = row ,node.col = col,node.relaxed=false,node.dist=Infinity,node.id= row*container.width/NODE_SIZE+col}
             Util.addStyel(node,'node')
             container.append(node)
             NODE.push(node)
@@ -19,11 +20,12 @@ function handleDraw(NODES) {
     }
     window.addEventListener('mousedown', e => {
         mouse.ispressed = true
-        // console.log(mouse.ispressed)
+        console.log(mouse.ispressed)
+         
     })
     window.addEventListener('mouseup', e => {
         mouse.ispressed = false
-        // console.log(mouse.ispressed)
+        console.log(mouse.ispressed)
     })
     for(var i = 0; i<NODES.length; i++){
         let node = NODES[i]
