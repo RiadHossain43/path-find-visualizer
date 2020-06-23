2e4cd0a8c68d6e9eb2ba1cfec2f28d3ac82f90f9
RiadHossain <riadhossain7464@gmail.com>
2020-06-23 22:12:41 +0600

    Update ui

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,24 @@
 import * as Util from './util.js'
 import { generateGrid, handleDraw } from './grid.js'
-import {apply} from './algorithm.js'
-import {help} from './help.js'
+import { apply } from './algorithm.js'
+import { help } from './help.js'
 let START = 0
 let DESTINATION = 0
+let NODES
 
 const container = Util.eleQRY('.container')
-const algo_btn = Util.eleQRY('.algo')
+const buttons = Util.eleCls('btns')
+const algo_btn = buttons[0]
+const reset_btn = buttons[1]
 
-function setNodeSIze(){
+function setNodeSize() {
     let NODE_SIZE
-    if(window.innerWidth<768) NODE_SIZE = 12
-    else NODE_SIZE = 15 
+    if (window.innerWidth < 768) NODE_SIZE = 12
+    else NODE_SIZE = 15
     {
         container.height = Math.floor((window.innerHeight - 50) / NODE_SIZE) * NODE_SIZE
         container.width = Math.floor((window.innerWidth - 20) / NODE_SIZE) * NODE_SIZE
-    }  
+    }
     Util.set_style(container, {
         height: `${container.height}px`, width: `${container.width}px`,
         gridTemplateColumns: `repeat(${Math.floor(container.width / NODE_SIZE)},${NODE_SIZE}px)`,
@@ -38,20 +41,20 @@ function selectStartToEnd() {
             return
         }
         if (points.startSelected && !points.endSelected) {
-            if(e.target!==NODES[START]){
+            if (e.target !== NODES[START]) {
                 Util.set_style(e.target, { backgroundColor: '#EF6C00' })
                 e.target.innerHTML = 'E'
                 points.endSelected = true
                 points.walldrawable = true
                 DESTINATION = e.target.id
                 console.log(NODES[START].id, NODES[DESTINATION].id)
-                return 
+                return
             }
         }
-        if (points.walldrawable) handleDraw(container,NODES)
+        if (points.walldrawable) handleDraw(container, NODES)
     })
 }
-function startAlgorithm(){
+function startAlgorithm() {
     algo_btn.addEventListener('click', () => {
         console.log(START, DESTINATION)
         NODES[START].dist = 0
@@ -60,12 +63,21 @@ function startAlgorithm(){
 }
 
 // initializing....
-help()
-let NODE_SIZE = setNodeSIze()
-let NODES = generateGrid(container, NODE_SIZE)
-console.log(NODES.length)
-selectStartToEnd()
-startAlgorithm()
+function start() {
+    START = 0
+    DESTINATION = 0
+    help()
+    let NODE_SIZE = setNodeSize()
+    NODES = generateGrid(container, NODE_SIZE)
+    console.log(NODES.length)
+    selectStartToEnd()
+    startAlgorithm()
+
+}
+start()
+// reset_btn.addEventListener('click',()=>{
+//     start()
+// })
 
 
 
@@ -73,20 +85,7 @@ startAlgorithm()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-export { START, DESTINATION ,NODES}
+export { START, DESTINATION, NODES }
 
 
 
