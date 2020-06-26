daff6ca2f0b2f67b7dea523e11d920bf71d3e7d6
RiadHossain <riadhossain7464@gmail.com>
2020-06-26 12:50:45 +0600

    New update

--- scripts/app.js
+++ scripts/app.js
@@ -32,7 +32,7 @@ function setNodeSize() {
     }
     else {
         NODE_SIZE = 14
-        heightbalance = 94
+        heightbalance = 95
     }
     {
         container.height = Math.floor((window.innerHeight - heightbalance) / NODE_SIZE) * NODE_SIZE
